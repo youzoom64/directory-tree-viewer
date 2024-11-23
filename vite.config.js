1180b738a3bacff540d46ba9d1b591c19925280f
youzoom64 <youzoom64@gmail.com>
2024-11-23 16:54:22 +0900

    Fix package.json and update dependencies 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ここを追加
+  base: process.env.NODE_ENV === 'production' ? '/directory-tree-viewer/' : '/',
 })
